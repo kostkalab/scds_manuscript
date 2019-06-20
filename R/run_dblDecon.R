31e87df0a5dc05b9a4efd903cf2e718fb4851f45
Dennis Kostka <kostka@pitt.edu>
2019-06-20 15:51:04 -0400

    Fixed dash/dot conversion issue with dblDecon

--- R/run_dblDecon.R
+++ R/run_dblDecon.R
@@ -20,7 +20,7 @@ run_dblDecon <- function(sce, species = "hsa"){
   seu <- RunPCA(seu, pc.genes = seu@var.genes, pcs.print = 0)
   seu <- FindClusters(seu)
   seu.markers <- FindAllMarkers(object = seu)
-  
+
   seu.markers = seu.markers[!grepl("\\.",rownames(seu.markers)),]
   seu.markers <- seu.markers %>% group_by(cluster) %>% top_n(50, avg_logFC)
 
@@ -31,9 +31,9 @@ run_dblDecon <- function(sce, species = "hsa"){
   exprsFile <- paste0(location, "input_exprs.txt")
   clustersFile <- paste0(location, "input_clusters.txt")
   genesFile <- paste0(location, "input_genes.txt")
-  write.table(expression, file = exprsFile, sep = "\t", row.names=T, col.names = NA, quote=F)
-  write.table(clusters, file = clustersFile, sep = "\t", row.names=T, col.names = NA, quote=F)
-  write.table(seu.markers, file = genesFile, sep = "\t", row.names=T, col.names = NA, quote=F)
+  write.table(expression, file = exprsFile, sep = "\t", row.names=TRUE, col.names = NA, quote=F)
+  write.table(clusters, file = clustersFile, sep = "\t", row.names=TRUE, col.names = NA, quote=F)
+  write.table(seu.markers, file = genesFile, sep = "\t", row.names=TRUE, col.names = NA, quote=F)
 
   newFiles <- Seurat_Pre_Process(exprsFile, genesFile, clustersFile)
 
@@ -56,8 +56,14 @@ run_dblDecon <- function(sce, species = "hsa"){
   dev.off()
   saveRDS(results, file = paste0(location, "results.rds"))
   doublets_res <- results$Final_doublets_groups
+
+  #- dash / dot confusion
+  if(all(sub("\\.", "",rownames(doublets_res)) %in%  gsub("-", "", rownames(colData(sce))) )) {
+          rownames(doublets_res) <- gsub("\\.", "-", rownames(doublets_res))}
+
   colData(sce)$dblDecon_call = rep("Singlet", ncol(sce))
   colData(sce)[rownames(doublets_res), ]$dblDecon_call = "Doublet"
+  colData(sce)$dblDecon_score = rep(NA,ncol(sce))
   metadata(sce)$dbl_decon_results = results
   return(sce)
 }
